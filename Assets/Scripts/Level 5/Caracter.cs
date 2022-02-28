e578460692e65b4c7d7759be9f736df41d21ef76
Aledrabaptism <aledra.baptism@gmail.com>
2022-02-27 21:24:09 -0600

    EJERCICIO 4 LEVEL 5

---
+++ Assets/Scripts/Level 5/Caracter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caracter : MonoBehaviour
+{
+    // al inicio se va a almacenar sin hacer una jecución del script.
+    public Sprite imagenCaracter;
+
+    public string nombreCaracter;
+}
